c654cfe328407f821cde9e6a9d140798bc356386
Suansh Budhiraja <43946155+ishaansharma750@users.noreply.github.com>
2023-05-30 00:48:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function deleteElement(){
+	let select = document.getElementsByTagName("select")[0];
+	let value = select.value;
+	let child = select.children;
+	for(let i=0;i<select.length;i++)
+		{
+			if(value === child[i].value)
+			{
+				child[i].remove();
+			}
+		}
+}
